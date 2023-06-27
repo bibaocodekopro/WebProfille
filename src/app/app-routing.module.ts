9cc443f1b1b106a76fc8c884c1c86f9cfbc46d9f
bibaocodekopro <95998397+bibaocodekopro@users.noreply.github.com>
2023-06-27 14:01:07 +0700

    ko lam nua

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  // {
-  //   path:'',
-  //   component: HomeComponent,
-  // },
- 
 ]
 
 @NgModule({
